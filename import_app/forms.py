cf31b4e913d70ced2e73d6df1f64597fa0373e46
shubhansh-simple <shubhansh7777@gmail.com>
2024-03-13 23:56:37 +0530

    Optimize speed by using empty label in ModelChoice instead O(N) loop

--- import_app/forms.py
+++ import_app/forms.py
@@ -8,11 +8,17 @@ from .models import Imports
 
 
 class ImportCreateForm(forms.ModelForm):
-    '''Form for importing items'''
+    '''Form for adding stock in current stock'''
 
     class Meta:
         model  = Imports
         fields = ['items','import_quantity'] 
+        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Update first item label in SELECT widget
+        self.fields['items'].empty_label = 'Choose Your Item'
 
 
 class ImportUpdateForm(forms.ModelForm):
