e05de5503f6358b7b4ed98a3ccd06c7fd636cf90
shubhansh-simple <shubhansh7777@gmail.com>
2024-03-13 21:06:30 +0530

    Remove import_by columns from Imports table

--- import_app/admin.py
+++ import_app/admin.py
@@ -12,7 +12,7 @@ class ImportsAdmin(ModelAdmin):
     '''Modify imports representation in admin site'''
 
     readonly_fields = ['id','import_date']
-    list_display    = ('items','id','import_by','import_quantity','import_date')
+    list_display    = ('items','id','import_quantity','import_date')
 
 
 # Register models for admin site
--- import_app/models.py
+++ import_app/models.py
@@ -3,6 +3,7 @@
 # django
 from django.db              import models
 from django.core.validators import MinValueValidator
+from django.utils           import timezone
 
 # local
 from items.models import Items
@@ -12,26 +13,34 @@ from django.conf  import settings
 class Imports(models.Model):
     '''Canteen Import Stock Model'''
 
-    items           = models.ForeignKey(Items,                    on_delete=models.PROTECT, verbose_name='Item to be imported', limit_choices_to={'is_active':True})
-    import_by       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, verbose_name='Imported By')
-    import_quantity = models.PositiveSmallIntegerField('Import Quantity', validators=[MinValueValidator(1)])
+    items           = models.ForeignKey(Items, 
+                                        on_delete=models.PROTECT, 
+                                        verbose_name='Item Imported', 
+                                        help_text='Please choose a item to import',
+                                        limit_choices_to={'is_active':True})
+
+    import_quantity = models.PositiveSmallIntegerField('Import Quantity', 
+                                                       validators=[MinValueValidator(1)], 
+                                                       help_text='Please enter a quantity greater than or equal to 1')
+
     import_date     = models.DateField(auto_now_add=True)
 
+
     class Meta:
-        # Avoid duplicate entry of a item in same date
-        unique_together     = ['items','import_date']
         verbose_name        = 'Import'
         verbose_name_plural = 'Imports'
+        ordering            = ['-import_date']
+
+        # Avoid duplicate entry of a item in same date
+        unique_together     = ['items','import_date']        # will raise IntegrityError
 
 
     def save(self, *args, **kwargs):
 
+        '''Gives trouble in updates'''
         #Total quantity in Items model should incremented by import_quantity
-        self.items.total_quantity += self.import_quantity
-        self.items.save()
-
-        self.import_date = timezone.now().date() + timezone.timedelta(1)
-
+        #self.items.total_quantity += self.import_quantity
+        #self.items.save()
         super().save(*args, **kwargs)
 
 
