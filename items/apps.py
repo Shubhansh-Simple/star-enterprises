5f2e2661046a6e75af14721325be630256c9e148
shubhansh-simple <shubhansh7777@gmail.com>
2024-03-18 06:03:58 +0530

    Rename apps.py name for admin site

--- items/apps.py
+++ items/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class ItemsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name         = 'items'
-    verbose_name = 'Canteen Items'
+    verbose_name = 'Canteen Current Stock'
