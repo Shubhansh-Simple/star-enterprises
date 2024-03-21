08e65fe40648634581aa27ae15d4375579f44a43
shubhansh-simple <shubhansh7777@gmail.com>
2024-03-21 17:04:05 +0530

    Sample Report-Detail view is ready

--- report_app/views.py
+++ report_app/views.py
@@ -1,8 +1,9 @@
 # report_app/views.py
 
 # django
-from django.views.generic import ListView, DetailView
+from django.http          import Http404
 from django.shortcuts     import get_object_or_404
+from django.views.generic import ListView, DetailView
 
 # local
 from .models                 import Reports
@@ -26,19 +27,30 @@ class DailyReportListView(ListView):
 
 # URL - /report/<str:entry_date>/
 class DailyReportDetailView(DetailView):
-    ''''''
+    '''Show report details of given date'''
 
     model               = Reports
     template_name       = 'daily-report-detail.html'
     context_object_name = 'daily_report_detail'
 
-
     def get_object(self, queryset=None):
-        '''Returns entries details of provided dates'''
+        '''Return report details of provided dates'''
 
         # Validate provided entry date
         entry_date = validate_entry_date( self.kwargs['entry_date'] )
 
-        get_object_or_404(Reports,)
+        if entry_date:
+            obj_report = Reports.objects.filter(entry_date=entry_date)
+            return obj_report
+        raise Http404
 
-        return super().get_object(queryset)
+    def get_context_data(self, **kwargs):
+        '''Adding Entry-date'''
+
+        context = super().get_context_data(**kwargs)
+        report  = self.get_object()
+
+        # Entry date
+        context['entry_date'] = report[0].entry_date if report else self.kwargs['entry_date']
+
+        return context
